e10aa11273d1980c66d342a1d610ca4fdf72ef4a
osvaldo gratereaux <osvaldo.nypl@gmail.com>
2022-01-18 20:49:56 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 function isPalindrome(word) {
   // Write your algorithm here
+   const letterArray = word.split('');
+   const letterReverse = letterArray.reverse()
+   const reverseString = letterReverse.join('')
+
+   if (word == reverseString) {
+     return true
+   } else {
+     return false
+   }
 }
 
 /* 
   Add your pseudocode here
+  The function has to 
+  take a string and
+  return true;
+  if not, the
+  function should
+  return a false.
 */
 
 /*
   Add written explanation of your solution here
+  The function takes 
+  the word and splits it 
+  into an array, and the 
+  array's then reversed. 
+  The array is then re-joined again. 
+  Next, the original word is compared 
+  to the reversed word. Finally, if it is true, 
+  then it returns true, or else it returns a false.
 */
 
 // You can run `node index.js` to view these console logs
